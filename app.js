9b82307ea561825efc82580dcf403321dadb6869
lauren-m-johnson <laurenmj90@gmail.com>
2023-06-16 17:14:41 -0700

    start js

---
+++ app.js
@@ -0,0 +1,37 @@
+/*----- constants -----*/
+const FRUITS = {
+    seven : {img: 'images/Seven.png', value: '0'},
+    cherries : {img: 'images/Cherries.png', value: '1'},
+    lemon : {img: 'images/Lemon.png', value: '2'},
+    watermelon : {img: 'images/Watermelon.png', value: '3'},
+}
+
+const winningCOMBOS = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3]
+]
+
+/*----- state variables -----*/
+let slotMachine;
+let bank;
+let bet;
+
+/*----- cached elements  -----*/
+const slot1 = document.getElementById('slot1');
+const slot2 = document.getElementById('slot2');
+const slot3 = document.getElementById('slot3');
+
+bank = document.getElementById('bank-amount');
+bet = document.getElementById('bet-amount');
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+function init() {
+    
+}
